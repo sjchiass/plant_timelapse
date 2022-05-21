5c6195d711225dbaa341c12417bc045859a1b232
sjchiass <sjchiass@gmail.com>
2022-05-21 14:32:39 -0400

    Fix data and add check

--- log_sensors.py
+++ log_sensors.py
@@ -4,6 +4,15 @@ from datetime import datetime
 ser = serial.Serial("/dev/ttyACM0", timeout=30)
 
 with open("/home/pi/plant_repo/sensor_data/data.csv", "a+") as f:
-    f.write(datetime.now().isoformat())
-    f.write(",")
-    f.write(ser.readline().decode("utf-8"))
+    out = datetime.now().isoformat()
+    out += ","
+    out += ser.readline().decode("utf-8")
+    if len(out) != 46:
+        print(f"ERROR: output not the expected length ({len(out)} vs. 46)")
+        print(f"OUTPUT: {out}")
+    elif out[-1] == "\r\n":
+        print(f"ERROR: output does not end with newline")
+        print(f"OUTPUT: {out}")
+    else:
+        f.write(out)
+        
