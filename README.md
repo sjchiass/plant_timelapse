0a121a640494308f0a4a36426e7837baedd19b33
sjchiass <sjchiass@users.noreply.github.com>
2022-05-23 01:23:53 +0000

    deploy: bd897fb62091f190707409c6bd68e693ec795a00

--- README.md
+++
@@ -1,5 +0,0 @@
-# Catgrass Timelapse
-
-These scripts take photos of growing catgrass and post them here.
-
-You can access the page at https://sjchiass.github.io/plant_timelapse/index.html
--- timelapse.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd /home/pi/plant_repo
-
-raspistill -o ./plant_stills/plant_$(date +%FT%T).jpg --annotate 12 --width 1280 --height 720 --quality 10 --rotation 90 --shutter 5000
-
-python3 jinja_template.py
