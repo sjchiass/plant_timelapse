ae61a4dd807070049c3fce51429e373156a4eec0
Sebastien Chiasson <sjchiass@gmail.com>
2022-05-23 08:24:37 -0400

    Add comments, fix 24h, fix sorting

--- jinja_template.py
+++ jinja_template.py
@@ -30,8 +30,11 @@ def extract_time(file, pattern):
 # Build a dataframe with the extracted times
 files_df = pd.DataFrame({"filename" : files,
                          "raw_time" : [extract_time(x, pattern) for x in files]})
+files_df["dt"] = pd.to_datetime(files_df.raw_time)
+files_df = files_df.set_index("dt")
+files_df = files_df.sort_index()
 
-# Get the latest time
+# Get the latest time, to display on the page
 latest_datetime = files_df.raw_time.tail(1).item()
 
 # Get the hour of each file
@@ -40,14 +43,16 @@ files_df["hour"] = pd.to_datetime(files_df.raw_time).dt.floor("H")
 # Get the last 12 hours, keep the first image for each hour
 files_df = files_df.groupby("hour").first().last("12h")
 
-# Get the image filenames into a list
+# Get the image filenames into a list so that the website can refer to
+# them all.
 filenames = files_df.filename.tolist()
 
-# Copy these images for the website to display
+# Copy these images for the website to use
 for f in filenames:
   shutil.copyfile(f"./plant_stills/{f}", f"./public/{f}")
 
 ### Sensor Data ###
+# Read in data, add column headers, and set a datetime index
 sensors_df = pd.read_csv("./sensor_data/data.csv",
   header=0,
   names=["time", "soil", "humidity", "temperature"])
@@ -55,44 +60,65 @@ sensors_df["dt"] = pd.to_datetime(sensors_df.time)
 sensors_df = sensors_df.set_index("dt")
 sensors_df = sensors_df.sort_index()
 
+### Indicators ###
+# Indicators are often used for KPIs, here we use them to summarise
+# changes in sensor readings in the past 6 hours
+# Determine the latest data and that of 6 hours ago
 latest = sensors_df.tail(1)
 previous = sensors_df.last("6h").head(1)
 
 fig = go.Figure()
 
-for n, (title, column) in enumerate(
-    zip(["Soil moisture", "Humidity", "Temperature"],
-        ["soil", "humidity", "temperature"])):
+# For column, we want an indicator with a specific title
+# The enumerate is used to place each indicator in a column in the grid
+variables = ["Soil moisture", "Humidity", "Temperature"]
+titles = ["soil", "humidity", "temperature"]
+for n, (t, v) in enumerate(zip(variables, titles)):
     fig.add_trace(go.Indicator(
-        title = title,
-        value = latest[column].item(),
+        title = t,
+        value = latest[v].item(),
         mode = "number+delta",
-        delta = {'reference': previous[column].item()},
+        delta = {'reference': previous[v].item()},
         gauge = {
             'axis': {'visible': False}},
         domain = {'row': 0, 'column': n}))
 
+# Define the grid and make the indicators smaller
 fig.update_layout(title="Sensor readings, compared to 6 hours ago",
-                  grid = {'rows': 1, 'columns': 3, 'pattern': "independent"},
+                  grid = {'rows': 1, 'columns': len(variables), 'pattern': "independent"},
                   width=800, height=300)
 
+# Save the plot as a JSON string so that it can be used with plotly JS
 indicators_json = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
+### Time series ###
+# Extract the last 24 hours
 last_24h = sensors_df.last("24h")
 
+# Specify that the graph will have a secondary axis
+# The main axis will be in % and the secondary axis degrees celsius
 fig = make_subplots(specs=[[{"secondary_y": True}]])
 
-fig.add_trace(go.Scatter(x=last_24h.index, y=sensors_df.soil, name="Soil moisture"), secondary_y=False)
+# Add each trace
+fig.add_trace(go.Scatter(x=last_24h.index, y=last_24h.soil, name="Soil moisture"), secondary_y=False)
 fig.add_trace(go.Scatter(x=last_24h.index, y=last_24h.humidity, name="Humidity"), secondary_y=False)
 fig.add_trace(go.Scatter(x=last_24h.index, y=last_24h.temperature, name="Temperature"), secondary_y=True)
 
+# Add axis titles and set decimal places
 fig.update_yaxes(title_text="Percentage", secondary_y=False, tickformat=".1f")
 fig.update_yaxes(title_text="Celsius", secondary_y=True, tickformat=".1f")
+
+# Change the hovermode to "x unified" which hovers over all variables at
+# once. Also, use the "simple_white" theme that gets rid of all grid lines.
 fig.update_layout(title="Sensor readings over past 24 hours",
                   hovermode="x unified", width=800, height=300, template="simple_white")
 
+# Save the plot as a JSON string so that it can be used with plotly JS
 time_series_json = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
+### Applying the template ###
+# Jinja lets us fill in spots of an html template file. This way various
+# information is put into the webpage
 index_template = (jinja2.Environment(
   loader=jinja2.FileSystemLoader("./")
   )
@@ -104,5 +130,6 @@ index_template = (jinja2.Environment(
           time_series=time_series_json)
   )
 
+# Save the html
 with open("./public/index.html", "w") as f:
   f.write(index_template)
