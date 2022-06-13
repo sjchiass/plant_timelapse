1105b9a6138f752ca521c721e130ca42df740b5f
sjchiass <sjchiass@gmail.com>
2022-06-13 08:20:13 -0400

    Took out -vf

--- timelapse.sh
+++ timelapse.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd "${0%/*}"
 
-raspistill -o ./plant_stills/plant_$(date +%FT%T).jpg --annotate 12 --width 1280 --height 720 --quality 10 -vf --shutter 5000
+raspistill -o ./plant_stills/plant_$(date +%FT%T).jpg --annotate 12 --width 1280 --height 720 --quality 10 --shutter 5000
